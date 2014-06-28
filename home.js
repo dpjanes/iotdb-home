90aba802741dbddc5733fd82cecd4f747fa07756
David P. Janes <davidjanes@davidjanes.com>
2014-06-28 08:33:54 -0400

    read all possible drivers

--- home.js
+++ home.js
@@ -600,7 +600,7 @@ if (1) {
 
 if (1) {
     var iot = new iotdb.IOT({ 
-        models_path: "node_modules/iotdb-models/ots",
+        models_path: "node_modules/iotdb-models//",
         load_models: true,
         load_drivers: true,
         iotdb_places_get: true,
